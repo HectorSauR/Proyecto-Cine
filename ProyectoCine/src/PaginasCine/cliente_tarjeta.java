de4a6e4e9d3e6521ff8b00bfa782266ab5aaf490
AzorStar <rodriguezurb28@gmail.com>
2022-04-29 18:32:22 -0600

    cambios del eclipse

---
+++ ProyectoCine/src/PaginasCine/cliente_tarjeta.java
@@ -0,0 +1,43 @@
+package PaginasCine;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class cliente_tarjeta {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					cliente_tarjeta window = new cliente_tarjeta();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public cliente_tarjeta() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
