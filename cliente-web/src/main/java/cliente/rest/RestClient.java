89bb2cdd476685c2fa49cc622912e0285e3b346a
HectorSauR <hector.sauceda.01@gmail.com>
2022-05-04 23:27:23 -0600

    InicioCliente

---
+++ cliente-web/src/main/java/cliente/rest/RestClient.java
@@ -0,0 +1,43 @@
+package cliente.rest;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+import org.json.*;
+
+public class RestClient {
+
+	public static void main(String[] args) {
+		try {
+			//String urlRestService = "http://127.0.0.1:5000/productos/mostrar";
+			String urlRestService = "http://26.77.197.154:8000/empleados/mostrar";
+			
+			ClientConfig clientConfig = new DefaultClientConfig();
+			clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+			Client client = Client.create(clientConfig);
+			WebResource webResource = client.resource(urlRestService);
+			ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
+			
+			String resp = response.getEntity(String.class);
+			
+			JSONArray ja = new JSONArray(resp);
+			
+			JSONObject jo = ja.getJSONObject(0);
+			
+			System.out.println(jo.get("apellido1"));
+			
+			//
+			
+			System.out.println("Respuesta : ");
+			System.out.println(ja);
+			
+		}catch(JSONException err){
+			System.out.println("Exception : "+err.toString());
+		}
+
+	}
+
+}
