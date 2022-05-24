6762367fb7b421816e19282408658dbe3414de6d
HectorSauR <hector.sauceda.01@gmail.com>
2022-05-24 09:37:07 -0600

    aa

--- cliente-web/src/main/java/PaginasCine/EscogerAsiento.java
+++ cliente-web/src/main/java/PaginasCine/EscogerAsiento.java
@@ -66,7 +66,7 @@ public class EscogerAsiento {
 		panel_asientos.setLayout(null);
 		panel_asientos.setBackground(new Color(217, 217, 217));
 		
-		JMenuItem menu_asiento_libre_1 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_1 = new JMenuItem("");
 		menu_asiento_libre_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -79,7 +79,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_1.setBackground(new Color(217, 217, 217));
 		panel_asientos.add(menu_asiento_libre_1);
 		
-		JMenuItem menu_asiento_libre_2 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_2 = new JMenuItem("");
 		menu_asiento_libre_2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -91,7 +91,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_2.setBounds(96, 33, 51, 42);
 		panel_asientos.add(menu_asiento_libre_2);
 		
-		JMenuItem menu_asiento_libre_3 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_3 = new JMenuItem("");
 		menu_asiento_libre_3.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -103,7 +103,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_3.setBounds(157, 33, 51, 42);
 		panel_asientos.add(menu_asiento_libre_3);
 		
-		JMenuItem menu_asiento_libre_4 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_4 = new JMenuItem("");
 		menu_asiento_libre_4.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -115,7 +115,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_4.setBounds(218, 33, 51, 42);
 		panel_asientos.add(menu_asiento_libre_4);
 		
-		JMenuItem menu_asiento_libre_5 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_5 = new JMenuItem("");
 		menu_asiento_libre_5.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -127,7 +127,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_5.setBounds(279, 33, 51, 42);
 		panel_asientos.add(menu_asiento_libre_5);
 		
-		JMenuItem menu_asiento_libre_6 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_6 = new JMenuItem("");
 		menu_asiento_libre_6.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -139,7 +139,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_6.setBounds(340, 33, 51, 42);
 		panel_asientos.add(menu_asiento_libre_6);
 		
-		JMenuItem menu_asiento_libre_7 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_7 = new JMenuItem("");
 		menu_asiento_libre_7.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -151,7 +151,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_7.setBounds(401, 33, 51, 42);
 		panel_asientos.add(menu_asiento_libre_7);
 		
-		JMenuItem menu_asiento_libre_8 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_8 = new JMenuItem("");
 		menu_asiento_libre_8.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -163,7 +163,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_8.setBounds(462, 33, 51, 42);
 		panel_asientos.add(menu_asiento_libre_8);
 		
-		JMenuItem menu_asiento_libre_9 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_9 = new JMenuItem("");
 		menu_asiento_libre_9.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -175,7 +175,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_9.setBounds(35, 127, 51, 42);
 		panel_asientos.add(menu_asiento_libre_9);
 		
-		JMenuItem menu_asiento_libre_10 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_10 = new JMenuItem("");
 		menu_asiento_libre_10.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -187,7 +187,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_10.setBounds(96, 127, 51, 42);
 		panel_asientos.add(menu_asiento_libre_10);
 		
-		JMenuItem menu_asiento_libre_11 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_11 = new JMenuItem("");
 		menu_asiento_libre_11.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -199,7 +199,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_11.setBounds(157, 127, 51, 42);
 		panel_asientos.add(menu_asiento_libre_11);
 		
-		JMenuItem menu_asiento_libre_12 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_12 = new JMenuItem("");
 		menu_asiento_libre_12.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -211,7 +211,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_12.setBounds(218, 127, 51, 42);
 		panel_asientos.add(menu_asiento_libre_12);
 		
-		JMenuItem menu_asiento_libre_13 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_13 = new JMenuItem("");
 		menu_asiento_libre_13.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -223,7 +223,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_13.setBounds(279, 127, 51, 42);
 		panel_asientos.add(menu_asiento_libre_13);
 		
-		JMenuItem menu_asiento_libre_14 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_14 = new JMenuItem("");
 		menu_asiento_libre_14.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -235,7 +235,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_14.setBounds(340, 127, 51, 42);
 		panel_asientos.add(menu_asiento_libre_14);
 		
-		JMenuItem menu_asiento_libre_15 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_15 = new JMenuItem("");
 		menu_asiento_libre_15.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -247,7 +247,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_15.setBounds(401, 127, 51, 42);
 		panel_asientos.add(menu_asiento_libre_15);
 		
-		JMenuItem menu_asiento_libre_16 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_16 = new JMenuItem("");
 		menu_asiento_libre_16.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -259,7 +259,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_16.setBounds(462, 127, 51, 42);
 		panel_asientos.add(menu_asiento_libre_16);
 		
-		JMenuItem menu_asiento_libre_17 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_17 = new JMenuItem("");
 		menu_asiento_libre_17.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -271,7 +271,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_17.setBounds(35, 223, 51, 42);
 		panel_asientos.add(menu_asiento_libre_17);
 		
-		JMenuItem menu_asiento_libre_18 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_18 = new JMenuItem("");
 		menu_asiento_libre_18.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -283,7 +283,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_18.setBounds(96, 223, 51, 42);
 		panel_asientos.add(menu_asiento_libre_18);
 		
-		JMenuItem menu_asiento_libre_19 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_19 = new JMenuItem("");
 		menu_asiento_libre_19.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -295,7 +295,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_19.setBounds(157, 223, 51, 42);
 		panel_asientos.add(menu_asiento_libre_19);
 		
-		JMenuItem menu_asiento_libre_20 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_20 = new JMenuItem("");
 		menu_asiento_libre_20.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -308,7 +308,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_20.setBounds(218, 223, 51, 42);
 		panel_asientos.add(menu_asiento_libre_20);
 		
-		JMenuItem menu_asiento_libre_21 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_21 = new JMenuItem("");
 		menu_asiento_libre_21.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -320,7 +320,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_21.setBounds(279, 223, 51, 42);
 		panel_asientos.add(menu_asiento_libre_21);
 		
-		JMenuItem menu_asiento_libre_22 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_22 = new JMenuItem("");
 		menu_asiento_libre_22.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -332,7 +332,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_22.setBounds(340, 223, 51, 42);
 		panel_asientos.add(menu_asiento_libre_22);
 		
-		JMenuItem menu_asiento_libre_23 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_23 = new JMenuItem("");
 		menu_asiento_libre_23.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -344,7 +344,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_23.setBounds(401, 223, 51, 42);
 		panel_asientos.add(menu_asiento_libre_23);
 		
-		JMenuItem menu_asiento_libre_24 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_24 = new JMenuItem("");
 		menu_asiento_libre_24.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -356,7 +356,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_24.setBounds(462, 223, 51, 42);
 		panel_asientos.add(menu_asiento_libre_24);
 		
-		JMenuItem menu_asiento_libre_25 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_25 = new JMenuItem("");
 		menu_asiento_libre_25.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -368,7 +368,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_25.setBounds(35, 321, 51, 42);
 		panel_asientos.add(menu_asiento_libre_25);
 		
-		JMenuItem menu_asiento_libre_26 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_26 = new JMenuItem("");
 		menu_asiento_libre_26.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -380,7 +380,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_26.setBounds(96, 321, 51, 42);
 		panel_asientos.add(menu_asiento_libre_26);
 		
-		JMenuItem menu_asiento_libre_27 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_27 = new JMenuItem("");
 		menu_asiento_libre_27.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -393,7 +393,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_27.setBounds(157, 321, 51, 42);
 		panel_asientos.add(menu_asiento_libre_27);
 		
-		JMenuItem menu_asiento_libre_28 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_28 = new JMenuItem("");
 		menu_asiento_libre_28.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -405,7 +405,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_28.setBounds(218, 321, 51, 42);
 		panel_asientos.add(menu_asiento_libre_28);
 		
-		JMenuItem menu_asiento_libre_29 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_29 = new JMenuItem("");
 		menu_asiento_libre_29.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -417,7 +417,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_29.setBounds(279, 321, 51, 42);
 		panel_asientos.add(menu_asiento_libre_29);
 		
-		JMenuItem menu_asiento_libre_30 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_30 = new JMenuItem("");
 		menu_asiento_libre_30.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -429,7 +429,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_30.setBounds(340, 321, 51, 42);
 		panel_asientos.add(menu_asiento_libre_30);
 		
-		JMenuItem menu_asiento_libre_31 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_31 = new JMenuItem("");
 		menu_asiento_libre_31.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -441,7 +441,7 @@ public class EscogerAsiento {
 		menu_asiento_libre_31.setBounds(401, 321, 51, 42);
 		panel_asientos.add(menu_asiento_libre_31);
 		
-		JMenuItem menu_asiento_libre_32 = new JMenuItem("");
+		final JMenuItem menu_asiento_libre_32 = new JMenuItem("");
 		menu_asiento_libre_32.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
