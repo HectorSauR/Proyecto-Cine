fc1efc62f9b72230549d87ab89329a999f867edd
ADO09 <jai_car_or11@hotmail.com>
2022-05-30 22:43:56 -0500

    aaa

--- cliente-web/src/main/java/cliente/rest/RestClient.java
+++ cliente-web/src/main/java/cliente/rest/RestClient.java
@@ -11,35 +11,49 @@ import org.json.*;
 public class RestClient {
 
 	public static void main(String[] args) {
+		
+		
+		JSONObject vo2 = new JSONObject();
+		vo2.put("nombre", "palomitas");
+		vo2.put("cantidad", "12");
+		JSONObject vo3 = new JSONObject();
+		JSONArray ja = new JSONArray();
+		ja.put(vo2);
+		vo3.put("nombre", "palomitas");
+		vo3.put("cantidad", "12");
+		JSONObject vo = new JSONObject(); 
+		ja.put(vo3);
+		
+	
+		
+		vo.put("nombre", "chanate");
+		vo.put("categoria", "1");
+		vo.put("precio", "100");
+		vo.put("PRODUCTOS", ja);
+		vo.put("lenguaje", "2");
+		
+		
+		System.out.println(vo);
+		
+		
+		
 		try {
 			//String urlRestService = "http://127.0.0.1:5000/productos/mostrar";
 			//String urlRestService = "http://26.77.197.154:8000/empleados/mostrar";
 			
-			JSONObject vo = new JSONObject(); 
-			JSONObject vo2 = new JSONObject();
-			
-			vo2.put("nombre", "palomitas");
-			vo2.put("cantidad", "12");
-			
-			vo.put("nombre", "chanate");
-			vo.put("categoria", "1");
-			vo.put("precio", "100");
-			vo.put("PRODUCTOS", vo2);
-			vo.put("lenguaje", "2");
 			
 			
-			
-			String urlRestService = "http://26.77.197.154:8000/pelicula/registrar";
+			//String urlRestService = "http://26.77.197.154:8000/pelicula/registrar";
 			//String urlRestService = "http://127.0.0.1:5000/pelicula/registrar";
-			ClientConfig clientConfig = new DefaultClientConfig();
-			clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-			Client client = Client.create(clientConfig);
-			WebResource webResource = client.resource(urlRestService);
+			//ClientConfig clientConfig = new DefaultClientConfig();
+			//clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+			//Client client = Client.create(clientConfig);
+			//WebResource webResource = client.resource(urlRestService);
 			//ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
 			//System.out.println(vo);
 			
-			ClientResponse response = webResource.type("application/json").post(ClientResponse.class, vo.toString());
-			String resp = response.getEntity(String.class);
+			//ClientResponse response = webResource.type("application/json").post(ClientResponse.class, vo.toString());
+			//String resp = response.getEntity(String.class);
 			
 			//JSONArray ja = new JSONArray(resp);
 			
@@ -50,10 +64,10 @@ public class RestClient {
 			//
 			
 			//System.out.println("Respuesta : ");
-			System.out.println(resp);
+			//System.out.println(resp);
 			
 		}catch(JSONException err){
-			System.out.println("Exception : "+err.toString());
+			//System.out.println("Exception : "+err.toString());
 		}
 
 	}
