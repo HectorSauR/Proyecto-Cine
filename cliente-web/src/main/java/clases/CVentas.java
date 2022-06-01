f4b932a2d83999b8a85fde140f35145d3bb818b6
HectorSauR <hector.sauceda.01@gmail.com>
2022-05-31 20:19:10 -0600

    gg

---
+++ cliente-web/src/main/java/clases/CVentas.java
@@ -0,0 +1,20 @@
+package clases;
+import org.json.*;
+
+public class CVentas {
+	
+	JSONObject venta = new JSONObject();	
+	JSONArray productos = new JSONArray();
+	JSONArray combos = new JSONArray();
+	
+	public void agregarProducto(int id,int cantidad) {
+		JSONObject prd = new JSONObject();
+		
+		
+	}
+	
+	
+	
+	
+	
+}
