818a04b00507d4aaef34f6e9f814b12e687961e1
ADO09 <jai_car_or11@hotmail.com>
2022-09-19 22:34:08 -0500

    aaa

--- cliente-web/src/main/java/PaginasCine/funciones_get_put.java
+++ cliente-web/src/main/java/PaginasCine/funciones_get_put.java
@@ -10,7 +10,7 @@ public class funciones_get_put {
 	}
 	
 	//public static  String ip="26.77.197.154" ;
-	public static  String ip="192.168.43.215" ;
+	public static  String ip="192.168.43.215";
 
 
 
